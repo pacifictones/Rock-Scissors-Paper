dda4bdb2827f11927545f82271d766e963085f1e
pacifictones <brianwburk@gmail.com>
2022-02-22 14:55:48 +0000

    add and update code.js

---
+++ code.js
@@ -0,0 +1,83 @@
+let win = 0;
+let lose = 0;
+
+function computerPlay() {
+    let value = Math.floor(Math.random()*3) + 1;
+    if (value === 1) {
+        return ("ROCK");
+    } else if (value === 2) {
+        return ("PAPER");
+    } else {
+        return ("SCISSORS");
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+    if(playerSelection.toUpperCase() === "ROCK") {
+        if(computerSelection ==="ROCK"){
+            return "Tie";
+        } else if(computerSelection === "PAPER") {
+            lose += 1;
+            return "You Lose! Paper beats Rock!";
+        } else {
+            win += 1;
+            return "You Win! Rock beats Scissors!";
+        } 
+    } else if(playerSelection.toUpperCase() === "PAPER") {
+        if (computerSelection === "ROCK") {
+            win += 1;
+            return "You Win! Paper beats Rock!";
+        } else if(computerSelection === "PAPER") {
+            return "Tie";
+        } else {
+            lose += 1;
+            return "You Lose! Scissors beats Paper!";
+        }
+    } else if (playerSelection.toUpperCase() === "SCISSORS") {
+        if(computerSelection === "PAPER") {
+            win += 1;
+            return "You Win! Scissors beats Paper!";
+        } else if (computerSelection === "ROCK") {
+            lose += 1;
+            return "You lose! Rock beats Scissors";
+        } else {
+            return "Tie";
+        } 
+    }
+    console.log(win);
+    console.log(lose);
+
+}
+function game(){
+    for (let i = 0; i < 5; i++){
+        const playerSelection = prompt("Enter rock, scissors or paper!", '');
+        console.log(playerSelection);
+        const computerSelection = computerPlay();
+        console.log(computerSelection);
+            
+
+        alert(playRound(playerSelection, computerSelection));
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    console.log(win);
+    console.log(lose);
+    if(win>lose){
+        alert("You won best of 5!");
+    } else if (win<lose) {
+        alert("You lost best of 5!");
+
+    } else if (win === lose) {
+        alert("Tie game!");
+    } else {
+        alert("I'm lost...");
+    }
+}
+
+game();
+
+
+
+
+
+
